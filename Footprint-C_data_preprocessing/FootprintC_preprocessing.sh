bffe5ccdcf0959c944c670646bf06e4e94b369e1
xiaokunliu01 <82570847+xiaokunliu01@users.noreply.github.com>
2024-09-28 16:15:58 +0800

    Update FootprintC_preprocessing.sh

--- Footprint-C_data_preprocessing/FootprintC_preprocessing.sh
+++ Footprint-C_data_preprocessing/FootprintC_preprocessing.sh
@@ -1,1 +1,58 @@
+#!/bin/bash
 
+
+mkdir fastqc_R1r1/
+mkdir trimAdapt_R1r1/
+mkdir trimLk_R1r1/
+
+# fastqc
+fastqc $1'_'1.fq.gz -o fastqc_R1r1/
+fastqc $1'_'2.fq.gz -o fastqc_R1r1/
+
+# trim adapter
+/home/lxk/private/software/miniconda3/bin/trim_galore -j 7 -q 20 --phred33 --stringency 3 --length 20 -e 0.1 --paired $1'_'*.fq.gz --gzip -o trimAdapt_R1r1/ --path_to_cutadapt /home/lxk/private/software/miniconda3/bin/cutadapt
+
+# trim S-linker
+/home/lxk/private/software/miniconda3/bin/cutadapt -a file:/home/lxk/private/optionData/MicroC_S-linker.fa -A file:/home/lxk/private/optionData/MicroC_S-linker.fa --rename='{id};{r1.adapter_name} {comment}' -j 10 --minimum-length=10 -o trimLk_R1r1/$1'_'val_trimLk1_1.fq.gz -p trimLk_R1r1/$1'_'val_trimLk1_2.fq.gz trimAdapt_R1r1/$1'_'1_val_1.fq.gz trimAdapt_R1r1/$1'_'2_val_2.fq.gz > trimLk_R1r1/$1'_'report1.txt
+/home/lxk/private/software/miniconda3/bin/cutadapt -a file:/home/lxk/private/optionData/MicroC_S-linker.fa -A file:/home/lxk/private/optionData/MicroC_S-linker.fa --rename='{id};{r1.adapter_name} {comment}' -j 10 --minimum-length=10 -o trimLk_R1r1/$1'_'val_trimLk2_1.fq.gz -p trimLk_R1r1/$1'_'val_trimLk2_2.fq.gz trimLk_R1r1/$1'_'val_trimLk1_1.fq.gz trimLk_R1r1/$1'_'val_trimLk1_2.fq.gz > trimLk_R1r1/$1'_'report2.txt
+/home/lxk/private/software/miniconda3/bin/cutadapt -a file:/home/lxk/private/optionData/MicroC_S-linker_noAT.fa -A file:/home/lxk/private/optionData/MicroC_S-linker_noAT.fa --rename='{id};{r1.adapter_name} {comment}' -j 10 --minimum-length=10 -o trimLk_R1r1/$1'_'val_trimLk3_1.fq.gz -p trimLk_R1r1/$1'_'val_trimLk3_2.fq.gz trimLk_R1r1/$1'_'val_trimLk2_1.fq.gz trimLk_R1r1/$1'_'val_trimLk2_2.fq.gz > trimLk_R1r1/$1'_'report3.txt
+
+# fastqc
+fastqc trimLk_R1r1/$1'_'trimLk3_1.fq.gz -o fastqc_R1r1/
+fastqc trimLk_R1r1/$1'_'trimLk3_2.fq.gz -o fastqc_R1r1/
+
+# HiC-Pro
+mkdir HiCPro_Analysis_R1r1/
+mkdir HiCPro_Analysis_R1r1/data
+mkdir HiCPro_Analysis_R1r1/data/$1'_'val_trimLk3
+
+cp trimLk_R1r1/$1'_'val_trimLk3_1.fq.gz HiCPro_Analysis_R1r1/data/$1'_'val_trimLk3/
+cp trimLk_R1r1/$1'_'val_trimLk3_2.fq.gz HiCPro_Analysis_R1r1/data/$1'_'val_trimLk3/
+
+cp /home/lxk/private/optionData/DNaseC_config_hicpro_XY_mm.txt HiCPro_Analysis_R1r1/config_hicpro.txt
+
+cd HiCPro_Analysis_R1r1/
+
+hic-pro -c config_hicpro.txt -i data -o hicpro_results -s mapping -s quality_checks
+hic-pro -c config_hicpro.txt -i hicpro_results/bowtie_results/bwt2 -o hicpro_results -s proc_hic -s merge_persample -s quality_checks
+
+cd ..
+
+
+rm fastqc_R1r1/*.zip
+
+
+mkdir fragment_contact_pairs/
+cd fragment_contact_pairs/
+
+bamToBed -bedpe -i ../HiCPro_Analysis_R1r1/hicpro_results/bowtie_results/bwt2/$1'_'val_trimLk3/$1'_'val__hg38XY+mm10XY.bwt2pairs.bam | sort -k1,1 -k2,3n -k4,4 -k5,6n -S 9G | awk '{if($1==$4 && $2>$5){print $4 "\t" $5 "\t" $6 "\t" $1 "\t" $2 "\t" $3 "\t" $7 "\t" $8 "\t" $10 "\t" $9}else{print $0}}' | awk -F';' '{print $1 "\t" $2$3$4$5$6$7$8$9}' | sort -k1,6 -k8,8 -k10,11 -u -S 9G | awk '{if($1!~/chr[CLMT]/ && $4!~/chr[CLMT]/ && $8!="no_adapterno_adapterno_adapter") print $0}' > $1'.'pair
+
+cd ..
+
+
+# fragment_Length
+
+mkdir fragment_Length/
+cd fragment_Length/
+
+awk '{print $3-$2"\t"$6-$5}' ../fragment_contact_pairs/$1'.'pair > $1'.'length
